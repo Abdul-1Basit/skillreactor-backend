f4064a1b7d234e80af17cbd95f23ac91b0237620
abdulbasit <abdulbasitria@hotmail.com>
2023-03-29 01:40:33 +0500

    latest commit for vercelll

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ dotenv.config()
 // app.use(fileUpload({
 //   createParentPath: true
 // }));
-app.use(cors());
+app.use(cors()); 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
